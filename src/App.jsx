f08382eb691c8742664114b1e5d24f1f41bf3db9
Adam Smith <adam.fortune.084@gmail.com>
2025-05-29 12:21:21 -0400

    Front Commit2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [userVariant, setUserVariant] = useState({ bool: true, id: "" });
   //  API URI's
   // const BASE_URL = "http://localhost:3000/api";
-  const BASE_URL = "https://adam-1-jcrl.onrender.com";
+  const BASE_URL = "https://adam-1-jcrl.onrender.com/api";
   const getUsers = `${BASE_URL}/users`;
   const postUser = `${BASE_URL}/addUser`;
 
